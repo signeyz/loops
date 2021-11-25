53596af9e3acc8bd5ff98ca7cd19ce935d1ff329
Rob B <rvabcode@gmail.com>
2021-11-25 16:01:23 -0500

    added DO WHILE Loop

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
+// FOR loop
+
 const cars = ['Cybertruck', 'Model S Plaid', 'Roadster'];
 
 for (i = 0; i < cars.length; i++) {
   console.log(cars[i]);
-  if (cars[i] === 'a') {
+  if (cars[i] === 'Cybertruck') {
     break;
   }
 }
+
+// DO WHILE loop
+
+const fruits = ['apple', 'orange', 'pear'];
+
+let a = 0;
+
+do {
+  console.log(fruits[a]);
+  a++;
+} while (a < fruits.length);
